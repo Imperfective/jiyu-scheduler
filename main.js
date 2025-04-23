f5842405ec24a99e47251c5f58011d1c1f67a5d4
MinKyu, Kim <105408727+Imperfective@users.noreply.github.com>
2025-04-24 00:17:03 +0900

    Update main.js

--- main.js
+++ main.js
@@ -14,17 +14,15 @@ function createWindow () {
 }
 
 app.whenReady().then(() => {
-  // 빌드 시 asarUnpack으로 포함된 위치에 맞춰 경로 수정
   const exePath = path.join(process.resourcesPath, 'public', 'app_backend.exe');
 
   exec(`start "" "${exePath}"`, (err, stdout, stderr) => {
     if (err) {
-      console.error('❌ app_backend.exe 실행 오류:', err);
+      console.error('app_backend.exe 실행 오류:', err);
       return;
     }
   });
 
-  // Streamlit 서버가 실행되기를 기다린 후 창 열기
   setTimeout(() => {
     createWindow();
   }, 5000);
